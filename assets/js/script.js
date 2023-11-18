4f4a2264aa26ce22788cf277b50f0fbe32da0844
Dimitris Giannoulis <dimitrisgiannoulis.jg@gmail.com>
2023-11-18 12:37:41 +0000

    Adds descriptive comments to index.html and some selectors to script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,6 @@
 console.log("script.js is working");
+
+// ELEMENT SELECTORS
+const searchForm = document.querySelector('#search-form');
+const searchInput = document.querySelector('#search-input');
+const searchSubmit = document.querySelector('#search-button');
